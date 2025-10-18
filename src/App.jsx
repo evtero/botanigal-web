33991021654ec76595515d12e74c7e4f1d9e5904
evtero <evap2697@gmail.com>
2025-10-18 10:26:11 +0200

    Añadir Function URL al CI/CD

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,54 @@
 console.log("App loaded");
-import ProtectedRoute from "./components/ProtectedRoute";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import About from "./components/About";
+import Auth from "./components/Auth";
 import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+import Progress from "./components/Progress";
+import ProtectedRoute from "./components/ProtectedRoute";
 import Quiz from "./components/Quiz";
 import Score from "./components/Score";
-import Progress from "./components/Progress";
-import Auth from "./components/Auth";
-import About from "./components/About"; 
+// Deploy update trigger
 
 export default function App() {
   return (
-    
     <Router basename="/botanigal-web">
       <div className="background-svg"></div>
       <Navbar />
       <Routes>
         <Route path="/" element={<Auth />} />
-        <Route path="/menu" element={<ProtectedRoute><Menu /></ProtectedRoute>} />
-        <Route path="/quiz" element={<ProtectedRoute><Quiz /></ProtectedRoute>} />
-        <Route path="/score" element={<ProtectedRoute><Score /></ProtectedRoute>} />
-        <Route path="/progress" element={<ProtectedRoute><Progress /></ProtectedRoute>} />
+        <Route
+          path="/menu"
+          element={
+            <ProtectedRoute>
+              <Menu />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/quiz"
+          element={
+            <ProtectedRoute>
+              <Quiz />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/score"
+          element={
+            <ProtectedRoute>
+              <Score />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/progress"
+          element={
+            <ProtectedRoute>
+              <Progress />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/about" element={<About />} />
       </Routes>
     </Router>
